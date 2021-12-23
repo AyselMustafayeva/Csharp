7b00634d4a61630c40e86d17bca54ed0f0b80cb4
Aysel Mustafayeva <91191069+AyselMustafayeva@users.noreply.github.com>
2021-12-23 20:07:37 +0300

    Add files via upload
    Birinci mesele

---
+++ Lesson-1 [22 dekabr 2021]/orta_eded/orta_eded/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace orta_eded
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void orta()
+            {
+                Console.WriteLine("Regemlerin sayini dahil et: ");
+                int say = Convert.ToInt32(Console.ReadLine());
+                double summa = 0;
+                double ortaeded = 0;
+                for(int i=0; i < say; i++)
+                {
+                    Console.WriteLine("Regemi dahil et: ");
+                    int regem = Convert.ToInt32(Console.ReadLine());
+                    summa=summa+ regem;
+                    ortaeded = summa / say;
+                }
+                Console.WriteLine("Orta eded = "+Math.Round(ortaeded,3));
+
+            }
+            orta();
+            Console.ReadLine();
+        }
+    }
+}
