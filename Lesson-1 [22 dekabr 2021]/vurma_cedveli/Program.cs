af87b2b90bcc09e84b8a144420f1bca4c62fb848
Aysel Mustafayeva <91191069+AyselMustafayeva@users.noreply.github.com>
2021-12-23 21:53:35 +0300

    Add files via upload

---
+++ Lesson-1 [22 dekabr 2021]/vurma_cedveli/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace vurma_cedveli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void vurma()
+            {
+                Console.WriteLine("Regemi dahil et: ");
+                int regem = Convert.ToInt32(Console.ReadLine());
+                int regem2 = 1;
+                for(int i = 1; i <= regem; i++)
+                {
+                    regem2*= i;   
+                }
+                Console.WriteLine("Vurma cedveli: "+regem2);
+            }
+            vurma();
+            Console.ReadLine();
+        }
+    }
+}
