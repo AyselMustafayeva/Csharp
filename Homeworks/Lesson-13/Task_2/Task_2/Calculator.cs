5fb14c8462620c2122e868bb85e1400163626518
Aysel Mustafayeva <mustafayevaaysel25@gmail.com>
2022-02-18 19:51:48 +0300

    Homework

---
+++ Homeworks/Lesson-13/Task_2/Task_2/Calculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    interface ISum
+    {
+        public double Sum(params double[] numbers)
+        {
+            double summa=0;
+            foreach(double num in numbers)
+            {
+                summa += num;
+            }
+            return summa;
+        }
+    }
+    interface IMultiply
+    {
+        public double Vurma(params double[] numbers)
+        {
+            double vurma = 1;
+            foreach (double num in numbers)
+            {
+                vurma *= num;
+            }
+            return vurma;
+        }
+    }
+    interface IDifference
+    {
+        public double Cixma(params double[] numbers)
+        {
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+            double cixma = numbers[0];
+            for(int i = 1; i < numbers.Length; i++)
+            {
+                cixma -= numbers[i];
+            }
+            return cixma;
+        }
+    }
+    interface IDivide
+    {
+        public double Bolme(params double[] numbers)
+        {
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+            double bolme = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                bolme /= numbers[i];
+            }
+            return bolme;
+        }
+    }
+
+    public class Calculator:ISum,IMultiply, IDifference, IDivide
+    {
+    }
+}
---
+++ Homeworks/Lesson-13/Task_2/Task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Interface_iwtetmeq
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("4 operasiya etmek olar, birini sec\n1.toplama\n2.vurma\n3.cixma\n4.bolme");
+        string secim = Console.ReadLine();
+        if (secim == "1")
+        {
+            Task_2.ISum calk1 = new Task_2.Calculator();
+            Console.WriteLine("Sum results: "+calk1.Sum(23, 54, 10, 14, 33, 6, 4));
+        }
+        else if (secim == "2")
+        {
+            Task_2.IMultiply calk1 = new Task_2.Calculator();
+            Console.WriteLine("Vurma results: " + calk1.Vurma(5, 4, 8, 10));
+        }
+        else if (secim == "3")
+        {
+            Console.WriteLine("Yeke regemnen balacani cixa cixa gedecek.");
+            Task_2.IDifference calk1 = new Task_2.Calculator();
+            Console.WriteLine("Cixmaq results: " + calk1.Cixma(21,19,33,88,200,20));
+        }
+        else if (secim == "4")
+        {
+            Console.WriteLine("Yeke regemnen balacani bole bole gedecek.");
+            Task_2.IDivide calk1 = new Task_2.Calculator();
+            Console.WriteLine("Bolmeq results: " + calk1.Bolme(3, 8, 20, 6, 800, 1,9,3,11));
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+    }
+}
