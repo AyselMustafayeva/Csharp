cd4bc74501d54ecdd2cc26177cb72ee59c0d80d6
Aysel Mustafayeva <mustafayevaaysel25@gmail.com>
2022-03-16 05:47:45 +0300

    [200~For_Admin for_Admin = Look;

--- Login-Registr/Login-Registr/Admin.Designer.cs
+++ Login-Registr/Login-Registr/Admin.Designer.cs
@@ -31,6 +31,10 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -73,12 +77,62 @@
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button4.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button4.Location = new System.Drawing.Point(43, 199);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(201, 38);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Look for user";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 152);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(26, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter the name, whom you look for : ";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button5.Location = new System.Drawing.Point(335, 139);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(217, 48);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Sort by registration time.";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(600, 350);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -86,6 +140,7 @@
             this.Text = "Admin";
             this.Load += new System.EventHandler(this.Admin_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -94,5 +149,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
     }
 }
--- Login-Registr/Login-Registr/Admin.cs
+++ Login-Registr/Login-Registr/Admin.cs
@@ -10,8 +10,12 @@ using System.Windows.Forms;
 
 namespace Login_Registr
 {
+    
     public partial class Admin : Form
     {
+        internal delegate string For_Admin(List<User> user_List);
+        internal string Name;
+
         public Admin()
         {
             InitializeComponent();
@@ -25,9 +29,11 @@ namespace Login_Registr
         private void button1_Click(object sender, EventArgs e)
         {
             string user = " ";
+            int index = 0;
             foreach (var all_user in User_list.users)
             {
-                user+=all_user.Name+" "+all_user.Surname+"  / "+all_user.Email+"\n";
+                index += 1;
+                user+= Convert.ToString(index)+". "+ all_user.Name + " " + all_user.Surname + "\nEmail: " + all_user.Email + "\n_______________\n";
             }
             
 
@@ -48,5 +54,52 @@ namespace Login_Registr
             Login login = new Login();
             login.Show();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            For_Admin for_Admin = Look;
+            string user_ss = for_Admin(User_list.users);
+            string title = "Users";
+            MessageBox.Show(user_ss, title);
+        }
+        public string Look(List<User> user_List)
+        {
+            string user_s=" ";
+            int index = 0;
+            foreach(var user in user_List)
+            {
+                if (Name == user.Name)
+                {
+                    index += 1;
+                    user_s += Convert.ToString(index) + ". " + user.Name + " " + user.Surname +"\nEmail: "+user.Email +"\n_______________\n";
+                    
+                }
+            }
+            return user_s;
+        }
+        public string Sorted(List<User> user_List)
+        {
+            string date = " ";
+            int inndex = 0;
+            foreach(var user in user_List)
+            {
+                inndex += 1;
+                date += Convert.ToString(inndex) + ". " + user.Name + " " + user.Surname + "            | Date: " + user.date.ToLongDateString() + "\n_______________\n";
+            }
+            return date;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Name = textBox1.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            For_Admin for_Admin2 = Sorted;
+            string user_date = for_Admin2(User_list.users);
+            string title = "Users";
+            MessageBox.Show(user_date, title);
+        }
     }
+    
 }
